e776f02db29885a7543d92aec49e91ee7afbeace
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-08-25 12:32:55 +0300

    add .gitignore and security refactoring

--- maincourse/settings.py
+++ maincourse/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from ..secrets import KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0l2g@0owq5a&g5a1t7#0z*&%by8@r4v3wlc5u=llnr4olh(@6!'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
